07c9fa1e548f634a1c90a26ecfeb98613811545a
zaidmillanes <zaidmillanes2@gmail.com>
2025-06-19 10:19:31 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allow access from any IP address
+    port: 5173,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
